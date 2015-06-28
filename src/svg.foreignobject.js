c8f1869f01980d1f7850316fbc8b99516699cb88
Florian Loch <florian.loch@gmail.com>
2015-06-28 19:26:52 +0200

    Make eslint smile again

--- src/svg.foreignobject.js
+++ src/svg.foreignobject.js
@@ -1,32 +1,36 @@
-SVG.ForeignObject = function() {
-  this.constructor.call(this, SVG.create('foreignObject'))
+(function () {
+  "use strict";
 
-  /* store type */
-  this.type = 'foreignObject';
-}
+  SVG.ForeignObject = function() {
+    this.constructor.call(this, SVG.create("foreignObject"));
 
-SVG.ForeignObject.prototype = new SVG.Shape()
+    /* store type */
+    this.type = "foreignObject";
+  };
 
-SVG.extend(SVG.ForeignObject, {
-  appendChild: function (child, attrs) {
-    var newChild = typeof(child)=='string' ? document.createElement(child) : child;
-    if (typeof(attrs)=='object'){
-      for(var a in attrs) {
-        var attrNode = document.createAttribute(a);
-        attrNode.nodeValue = attrs[a];
-        newChild.setAttributeNode(attrNode);
+  SVG.ForeignObject.prototype = new SVG.Shape();
+
+  SVG.extend(SVG.ForeignObject, {
+    appendChild: function (child, attrs) {
+      var newChild = (typeof child === "string") ? document.createElement(child) : child;
+      if (typeof attrs === "object"){
+        for(var a in attrs) {
+          var attrNode = document.createAttribute(a);
+          attrNode.nodeValue = attrs[a];
+          newChild.setAttributeNode(attrNode);
+        }
       }
+      this.node.appendChild(newChild);
+      return this;
+    },
+    getChild: function (index) {
+      return this.node.childNodes[index];
     }
-    this.node.appendChild(newChild);
-    return this;
-  },
-  getChild: function (index) {
-    return this.node.childNodes[index];
-  }
-});
+  });
 
-SVG.extend(SVG.Container, {
-  foreignObject: function(width, height) {
-    return this.put(new SVG.ForeignObject()).size(width === null ? 100 : width, height === null ? 100 : height)
-  }
-});
+  SVG.extend(SVG.Container, {
+    foreignObject: function(width, height) {
+      return this.put(new SVG.ForeignObject()).size((width === null) ? 100 : width, (height === null) ? 100 : height);
+    }
+  });
+})();
